c4dc7ef6dc7e5bbd06424e914fdb8bae7de95fea
Dmitrii Iglin <Dmitrii_Iglin@epam.com>
2020-09-18 12:31:57 +0300

    initial project structure

---
+++ docker/init.groovy.d/1_add_users.groovy
@@ -0,0 +1,24 @@
+import jenkins.model.*
+import hudson.security.*
+
+def env = System.getenv()
+
+def jenkins = Jenkins.getInstance()
+jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
+
+def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASS)
+user.save()
+
+jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
+
+jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, env.JENKINS_USER)
+
+def userAlice = jenkins.getSecurityRealm().createAccount("Alice", "AlicePassword")
+userAlice.save()
+def userBob = jenkins.getSecurityRealm().createAccount("Bob", "BobPassword")
+userBob.save()
+jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, "Alice")
+jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, "Bob")
+
+jenkins.save()
+print('users added successfully')
