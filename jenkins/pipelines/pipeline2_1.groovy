dca08f32b2cbc39d43c2c7d94011ef388d7981e7
d_iglin <d_iglin@onriva.com>
2020-09-22 16:25:07 +0300

    add pipelines for project 2

--- jenkins/pipelines/pipeline2_1.groovy
+++ jenkins/pipelines/pipeline2_1.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Hello stage') {
             steps { 
-                echo 'Hello from pipeline 2_1'
+                echo "Hello from pipeline 2_1 and environment: ${PROJECT_ENVIRONMENT}"
             }
         }
     }
--- jenkins/src/com/domain/project/dsl/jobs/createAutomationJobsProject2.groovy
+++ jenkins/src/com/domain/project/dsl/jobs/createAutomationJobsProject2.groovy
@@ -1,10 +1,16 @@
-pipelineJob('PROJECT_2/pipeline1') {
-    definition {
-        cpsScm {
-            scm {
-                git('$PROJECT_URL', '*/master', {node -> node / 'extensions' << '' })
-		scriptPath('jenkins/src/com/domain/project/dsl/factory/GenerateJobFactory1.groovy')
+def environments = ['TEST', 'DEV', 'PROD']
+
+for (String env_name : environments) {
+    pipelineJob('PROJECT_2/pipeline2_1_' + env_name) {
+	definition {
+            cpsScm {
+		scm {
+                    git("$PROJECT_URL", '*/master', {node -> node / 'extensions' << '' })
+		    scriptPath('jenkins/pipelines/pipeline2_1.groovy')
+		}
             }
-        }
+	}
+	environmentVariables(PROJECT_ENVIRONMENT: env_name)	
     }
 }
+
